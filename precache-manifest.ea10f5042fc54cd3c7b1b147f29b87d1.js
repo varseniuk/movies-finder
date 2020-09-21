ac9dec9894dd07e656c9502f7262cc7e1bc8700b
Vitalii Arseniuk <arsenyuk.v.v@gmail.com>
2020-09-21 11:05:19 +0300

    Updates

---
+++ precache-manifest.ea10f5042fc54cd3c7b1b147f29b87d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e86ecbc81a978eaa89c9248181bb5f5",
+    "url": "/movies-finder/index.html"
+  },
+  {
+    "revision": "6d8964a66c81b5885564",
+    "url": "/movies-finder/static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "0a3d3a40d8115460e475",
+    "url": "/movies-finder/static/css/main.bf49f72d.chunk.css"
+  },
+  {
+    "revision": "6d8964a66c81b5885564",
+    "url": "/movies-finder/static/js/2.8d83393b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies-finder/static/js/2.8d83393b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3d3a40d8115460e475",
+    "url": "/movies-finder/static/js/main.71bd0dcc.chunk.js"
+  },
+  {
+    "revision": "8465f6d3c8ad712d384b",
+    "url": "/movies-finder/static/js/runtime-main.c804091b.js"
+  }
+]);
